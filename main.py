a64d3ace328b337a0566c8fa92a312a6e22fbaa4
Roman <aspir5920@mail.ru>
2021-03-13 20:37:46 +0300

    Все работает

--- main.py
+++ main.py
@@ -1,17 +1,71 @@
-blocks = {"A": 2, "B": 7, "C": 12, 'D': 11}
+def findNextGoodKlaster(currentKlaster, klasters):
+    for i in range(currentKlaster,len(klasters)):
+        if klasters[currentKlaster+1]=='bad':
+            currentKlaster+=1
+        else:
+            return currentKlaster+1
+
+files = {"A": 2, "B": 7, "C": 12, 'D': 11}
 klasters = ['', '', 3, 4, 5, 'eof', 'eof', 16, 'eof', 8, 'bad', 13, 13, 20, "", 'bad', 9, "", "", "", 6, '', '', '', 25,
             26, 'eof', '']
-normalizedBlocks = {}
+normalizedFiles = {}
 
-for i in blocks:
-    normalizedBlocks[i] = []
-    normalizedBlocks[i].append(blocks[i])
-    currentClaster = blocks[i]
+for i in files:
+    normalizedFiles[i] = []
+    normalizedFiles[i].append(files[i])
+    currentClaster = files[i]
     while 1:
         if type(currentClaster) == int:
             if klasters[currentClaster]!="eof":
-                normalizedBlocks[i].append(klasters[currentClaster])
+                normalizedFiles[i].append(klasters[currentClaster])
             currentClaster = klasters[currentClaster]
         else:
             break
-print(normalizedBlocks)
+badKlasters=[]
+normalKlasters=[]
+for i in range(2,len(klasters)):
+    if klasters[i]!='bad':
+        normalKlasters.append(i)
+    if klasters[i]=='bad':
+        badKlasters.append(i)
+goodClasters=['' for i in range(len(klasters))]
+for i in badKlasters:
+    goodClasters[i]='bad'
+fileSizes= {}
+for i in normalizedFiles:
+    fileSizes[i]=len(normalizedFiles[i])
+
+
+
+
+
+
+currentClaster=2
+for i in fileSizes:
+    if currentClaster==len(goodClasters):
+        break
+    j=0
+    while j<fileSizes[i]:
+        if goodClasters[currentClaster] != 'bad': # если текущий кластер не бэд
+            if j==fileSizes[i]-1:
+                goodClasters[currentClaster] = 'eof' # если это последнй кластер файла
+                currentClaster += 1
+            else:
+                goodClasters[currentClaster]=findNextGoodKlaster(currentClaster,goodClasters)
+                currentClaster=findNextGoodKlaster(currentClaster,goodClasters)
+            j+=1
+        else:
+            currentClaster+=1
+
+normalFiles = {}
+currentClaster=2
+for i in normalizedFiles:
+    normalFiles[i]=[]
+    while goodClasters[currentClaster]!='eof':
+        if goodClasters[currentClaster]!='bad':
+            normalFiles[i].append(currentClaster)
+
+print(goodClasters)
+
+
+
