53e5ddc6193f3509e4ec3354e9bc5afe50c5a041
Roman <aspir5920@mail.ru>
2021-03-13 12:02:59 +0300

    Теперь нормальный вид

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-blocks={"A":2,"B":7,"C":12}
+blocks = {"A": 2, "B": 7, "C": 12, 'D': 11}
+klasters = ['', '', 3, 4, 5, 'eof', 'eof', 16, 'eof', 8, 'bad', 13, 13, 20, "", 'bad', 9, "", "", "", 6, '', '', '', 25,
+            26, 'eof', '']
+normalizedBlocks = {}
 
-klasters=['','',3,4,5,'eof','eof',16,'eof',8,'bad','',13,20,"",'bad',9,"","","",6]
-def getNextKlaster(i):
-    try:
-        print(klasters[i], end=' ')
-        if type(klasters[i])==int:
-            getNextKlaster(klasters[i])
-        else:
-            print("\n")
-    except:
-        pass
 for i in blocks:
-    print(str(i) +": " +str(blocks[i])+ " ",end="")
-    getNextKlaster(blocks[i])
+    normalizedBlocks[i] = []
+    normalizedBlocks[i].append(blocks[i])
+    currentClaster = blocks[i]
+    while 1:
+        if type(currentClaster) == int:
+            if klasters[currentClaster]!="eof":
+                normalizedBlocks[i].append(klasters[currentClaster])
+            currentClaster = klasters[currentClaster]
+        else:
+            break
+print(normalizedBlocks)
